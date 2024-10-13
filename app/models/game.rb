ca2d4b6ced6cba8c346b5c93bd13f5521f33dcec
Boris Knezevic <boris.knezevic0@gmail.com>
2024-10-13 15:06:16 +0200

    remove method to create frames on game creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,5 @@
 class Game < ApplicationRecord
     has_many :frames, dependent: :destroy
 
-    after_create :create_frames
     default_scope { includes(:frames) }
-
-    def create_frames
-        10.times do
-            Frame.create!(game: self)
-        end
-    end
 end
--- spec/models/frame_spec.rb
+++ spec/models/frame_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe Frame, type: :model do
   subject(:game) { Game.create }
   subject(:frame) { Frame.create(game: game, first_roll: 5, second_roll: 5) }
-  
-  before do
-    allow_any_instance_of(Game).to receive(:create_frames)
-  end
 
   describe 'validations' do
     it { is_expected.to validate_numericality_of(:first_roll) }
